44d1819976a7ee36207019d1d33b5cadf33eed96
JxxHxxx <leesin5498@naver.com>
2023-04-01 02:09:53 +0900

    initial commit

---
+++ src/main/java/com/jxx/xuni/XuniApplication.java
@@ -0,0 +1,13 @@
+package com.jxx.xuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuniApplication.class, args);
+    }
+
+}
