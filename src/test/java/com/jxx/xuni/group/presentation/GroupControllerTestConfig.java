30e09f6fa5782d5fae86c95ac5d3b75ae16f1136
JxxHxxx <leesin5498@naver.com>
2023-04-03 22:54:09 +0900

    Test/Controller 테스트 전용 인증 필터 (#6)

--- src/test/java/com/jxx/xuni/group/presentation/GroupControllerTestConfig.java
+++ src/test/java/com/jxx/xuni/group/presentation/GroupControllerTestConfig.java
@@ -1,5 +1,7 @@
 package com.jxx.xuni.group.presentation;
 
+import com.jxx.xuni.TestLoginFilter;
+import com.jxx.xuni.auth.config.LoginFilter;
 import com.jxx.xuni.group.application.GroupCreateService;
 import com.jxx.xuni.group.domain.GroupRepository;
 import com.jxx.xuni.group.domain.SimpleHostCreator;
@@ -9,15 +11,16 @@ import org.springframework.context.annotation.Bean;
 
 @TestConfiguration
 public class GroupControllerTestConfig {
-
     @MockBean
     GroupRepository groupRepository;
-
     @MockBean
     SimpleHostCreator simpleHostCreator;
-
     @Bean
     GroupCreateService groupCreateService() {
         return new GroupCreateService(groupRepository, simpleHostCreator);
+    }
+    @Bean
+    LoginFilter loginFilter() {
+        return new TestLoginFilter();
     }
 }
