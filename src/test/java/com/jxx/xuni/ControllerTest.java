82b1c08a3f98c24444c08a21b2f07cc1a55d6d5c
JxxHxxx <leesin5498@naver.com>
2023-04-03 15:49:44 +0900

    Test/Group Presentation 계층 테스트 (#4)

---
+++ src/test/java/com/jxx/xuni/ControllerTest.java
@@ -0,0 +1,15 @@
+package com.jxx.xuni;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jxx.xuni.group.presentation.GroupCreateController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(GroupCreateController.class)
+public class ControllerTest {
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    protected ObjectMapper objectMapper;
+}
