363106f328503f2fd18be840796cd6dbd6e0dd8e
JxxHxxx <leesin5498@naver.com>
2023-04-04 17:25:14 +0900

    Test/Mock 세션 주입, URI 변경 (#1)

--- src/test/java/com/jxx/xuni/TestLoginFilter.java
+++ src/test/java/com/jxx/xuni/TestLoginFilter.java
@@ -1,15 +1,22 @@
 package com.jxx.xuni;
 
+import com.jxx.xuni.auth.application.MemberDetails;
 import com.jxx.xuni.auth.config.LoginFilter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Slf4j
 public class TestLoginFilter extends LoginFilter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpSession session = httpRequest.getSession();
+        session.setAttribute("loginMember", new MemberDetails(1l, "leesin5498@naver.com", "이재헌"));
+
         chain.doFilter(request,response);
     }
 }
