6f15504b520b1e15d48452812a38a0b0d58008d5
JxxHxxx <leesin5498@naver.com>
2023-04-05 17:32:23 +0900

    Feat/순수 스프링 빈 테스트 환경 @ServiceOnly 애노테이션 (#13)

---
+++ src/test/java/com/jxx/xuni/support/ServiceOnlyTest.java
@@ -0,0 +1,21 @@
+package com.jxx.xuni.support;
+
+import com.jxx.xuni.common.support.ServiceOnly;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 순수하게 ServiceOnly - Repository 빈들만 테스트 하려는 경우
+ * 수동 등록, @Service 가 붙은 빈은 주입되지 않습니다.
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@DataJpaTest(includeFilters = @ComponentScan.Filter(classes = {ServiceOnly.class}))
+public @interface ServiceOnlyTest {
+}
