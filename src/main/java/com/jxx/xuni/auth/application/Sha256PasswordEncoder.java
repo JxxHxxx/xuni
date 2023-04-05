ca61f7b1d0ea4e2fa6dabdc51cc5a5d6e371d72f
JxxHxxx <leesin5498@naver.com>
2023-04-06 04:30:18 +0900

    Refactor/비밀번호 암호기 스프링 의존성 제거 (#17)

--- src/main/java/com/jxx/xuni/auth/application/Sha256PasswordEncoder.java
+++ src/main/java/com/jxx/xuni/auth/application/Sha256PasswordEncoder.java
@@ -1,18 +1,22 @@
 package com.jxx.xuni.auth.application;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Service
-@RequiredArgsConstructor
 public class Sha256PasswordEncoder implements PasswordEncoder{
 
-    private final MessageDigest messageDigest;
-
     @Override
-    public String encrypt(String password) {
+    public String encrypt(String password)  {
+        MessageDigest messageDigest;
+        try {
+            messageDigest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
         byte[] hash = messageDigest.digest(password.getBytes());
         StringBuilder stringBuilder = new StringBuilder();
         for (byte b : hash) {
