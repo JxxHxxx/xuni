4310defa09f1aeb07f481a864d6ed516d31ebff9
JxxHxxx <leesin5498@naver.com>
2023-04-06 16:31:47 +0900

    Feat/그룹 모집 완료 기능 프레젠테이션/애플리케이션 계층 구현 (#19)

--- src/main/java/com/jxx/xuni/group/dto/response/GroupApiSimpleResult.java
+++ src/main/java/com/jxx/xuni/group/dto/response/GroupApiSimpleResult.java
@@ -1,8 +1,8 @@
 package com.jxx.xuni.group.dto.response;
 
 import lombok.Getter;
-import static com.jxx.xuni.group.dto.response.GroupApiMessage.GROUP_CREATED;
-import static com.jxx.xuni.group.dto.response.GroupApiMessage.GROUP_JOIN;
+
+import static com.jxx.xuni.group.dto.response.GroupApiMessage.*;
 
 @Getter
 public class GroupApiSimpleResult {
@@ -20,5 +20,9 @@ public class GroupApiSimpleResult {
 
     public static GroupApiSimpleResult join() {
         return new GroupApiSimpleResult(200, GROUP_JOIN);
+    }
+
+    public static GroupApiSimpleResult closeRecruitment() {
+        return new GroupApiSimpleResult(200, GROUP_CLOSE_RECRUITMENT);
     }
 }
