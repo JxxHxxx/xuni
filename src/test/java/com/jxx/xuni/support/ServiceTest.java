af4d0ce0d3be4814b060b33f5aca63ed5b4c5b5d
JxxHxxx <leesin5498@naver.com>
2023-04-20 23:06:11 +0900

    Docs/@ServiceTest 주석 추가 (#35)

--- src/test/java/com/jxx/xuni/support/ServiceTest.java
+++ src/test/java/com/jxx/xuni/support/ServiceTest.java
@@ -10,6 +10,12 @@ import java.lang.annotation.Target;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
+/**
+ * @Repository - @Service 레이어를 테스트 합니다.
+ * 외부 라이브러리, MVC 의존이 필요한 클래스에 @Service 를 붙이면 @ServiceTest 에서 오류가 날 수 있습니다.
+ * 외부 라이브러리, MVC 의존이 필요한 클래스에는 @Component 를 붙이는 것을 권장합니다.
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @DataJpaTest(includeFilters = @Filter(classes = {Service.class}))
