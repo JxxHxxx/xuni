699c326c098e2a30607302a05f5854fa5839731e
JxxHxxx <leesin5498@naver.com>
2023-04-30 14:15:59 +0900

    Feat/스터디 상품 상세 등록 API 개발 (#69)
    주요 변화 studyProductDetail chapterId 컬럼 unique 속성 제거

--- src/main/java/com/jxx/xuni/studyproduct/presentation/StudyProductCreateController.java
+++ src/main/java/com/jxx/xuni/studyproduct/presentation/StudyProductCreateController.java
@@ -4,13 +4,18 @@ import com.jxx.xuni.auth.application.MemberDetails;
 import com.jxx.xuni.auth.presentation.AdminMember;
 import com.jxx.xuni.common.service.AmazonS3Handler;
 import com.jxx.xuni.studyproduct.application.StudyProductCreateService;
+import com.jxx.xuni.studyproduct.dto.request.StudyProductDetailForm;
 import com.jxx.xuni.studyproduct.dto.request.StudyProductForm;
+import com.jxx.xuni.studyproduct.dto.response.StudyProductApiSimpleResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,13 +28,23 @@ public class StudyProductCreateController {
     private String s3ImageOrigin;
 
     @PostMapping("/study-products")
-    public String enroll(@AdminMember MemberDetails memberDetails,
+    public ResponseEntity<StudyProductApiSimpleResult> enroll(@AdminMember MemberDetails memberDetails,
                            @RequestPart(value = "image", required = false) MultipartFile file,
                            @RequestPart("data") StudyProductForm form) throws IOException {
 
         String objectKey = amazonS3Handler.putS3Object(file);
         studyProductCreateService.create(form, s3ImageOrigin + objectKey);
 
-        return "스터디 상품 등록 완료";
+        return new ResponseEntity(StudyProductApiSimpleResult.create(), HttpStatus.CREATED);
+
+    }
+
+    @PostMapping("/study-products/{study-product-id}")
+    public ResponseEntity<StudyProductApiSimpleResult> createDetail(@AdminMember MemberDetails memberDetails,
+                                                                     @PathVariable("study-product-id") String studyProductId,
+                                                                     @RequestBody List<StudyProductDetailForm> StudyProductDetailForms) {
+
+        studyProductCreateService.createDetail(studyProductId, StudyProductDetailForms);
+        return new ResponseEntity(StudyProductApiSimpleResult.createDetail(), HttpStatus.CREATED);
     }
 }
