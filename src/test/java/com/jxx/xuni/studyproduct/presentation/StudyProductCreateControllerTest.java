aa0d594a33cb2b9d5b318451398270f1319d49d5
JxxHxxx <leesin5498@naver.com>
2023-04-30 14:17:56 +0900

    Test/스터디 상품 상세 등록 API 테스트 (#69)

--- src/test/java/com/jxx/xuni/studyproduct/presentation/StudyProductCreateControllerTest.java
+++ src/test/java/com/jxx/xuni/studyproduct/presentation/StudyProductCreateControllerTest.java
@@ -4,25 +4,33 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.jxx.xuni.auth.support.JwtTokenProvider;
 import com.jxx.xuni.common.service.AmazonS3Handler;
 import com.jxx.xuni.group.domain.TestGroupServiceSupporter;
+import com.jxx.xuni.studyproduct.application.StudyProductCreateService;
 import com.jxx.xuni.studyproduct.domain.Category;
+import com.jxx.xuni.studyproduct.dto.request.StudyProductDetailForm;
 import com.jxx.xuni.studyproduct.dto.request.StudyProductForm;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.payload.PayloadDocumentation;
+import org.springframework.restdocs.request.RequestDocumentation;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
 
 import static com.jxx.xuni.ApiDocumentUtils.*;
+import static com.jxx.xuni.studyproduct.dto.response.StudyProductApiMessage.STUDY_PRODUCT_CREATED;
+import static com.jxx.xuni.studyproduct.dto.response.StudyProductApiMessage.STUDY_PRODUCT_DETAIL_CREATED;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -32,6 +40,8 @@ class StudyProductCreateControllerTest extends StudyProductCommon{
     JwtTokenProvider jwtTokenProvider;
     @Autowired
     AmazonS3Handler amazonS3Handler;
+    @Autowired
+    StudyProductCreateService studyProductCreateService;
     byte[] byteForm;
 
     @BeforeEach
@@ -47,19 +57,27 @@ class StudyProductCreateControllerTest extends StudyProductCommon{
         String AdminToken = jwtTokenProvider.issue(TestGroupServiceSupporter.AdminMemberDetails(1l));
         MockMultipartFile data = new MockMultipartFile("data", "data", "application/json", byteForm);
 
-        ResultActions result = mockMvc.perform(RestDocumentationRequestBuilders.multipart("/study-products")
+        ResultActions result = mockMvc.perform(multipart("/study-products")
                 .file(data)
                 .header("Authorization", AdminToken));
 
-        result.andExpect(content().string("스터디 상품 등록 완료"))
-                .andDo(MockMvcRestDocumentation.document("studyproduct/create",
+        result
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(STUDY_PRODUCT_CREATED))
+
+                .andDo(MockMvcRestDocumentation.document("studyproduct/create/self",
                         getDocumentRequest(),
                         PayloadDocumentation.requestPartFields("data",
                                 fieldWithPath("name").type(JsonFieldType.STRING).description("스터디 이름"),
                                 fieldWithPath("category").type(JsonFieldType.STRING).description("스터디 상품 카테고리"),
                                 fieldWithPath("content").type(JsonFieldType.STRING).description("상품 이름"),
                                 fieldWithPath("author").type(JsonFieldType.STRING).description("스터디 상품 저자")
-                        )));
+                        ),
+                        PayloadDocumentation.responseFields(
+                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("상태 코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
+                        )
+                ));
     }
 
 
@@ -76,5 +94,44 @@ class StudyProductCreateControllerTest extends StudyProductCommon{
                         .header("Authorization", userToken))
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.message").value("권한이 존재하지 않습니다."));
+    }
+
+    @DisplayName("스터디 상품 상세 등록 성공")
+    @Test
+    void create_detail_success() throws Exception {
+        //given
+        String AdminToken = jwtTokenProvider.issue(TestGroupServiceSupporter.AdminMemberDetails(1l));
+        String studyProductId = "6f7e5d4a-4893-4efc-b8c4-fdda39cabd55";
+        List<StudyProductDetailForm> form = List.of(
+                new StudyProductDetailForm("spring IOC"),
+                new StudyProductDetailForm("spring AOP"),
+                new StudyProductDetailForm("spring MVC"));
+
+        //when
+        ResultActions result = mockMvc.perform(post("/study-products/{study-product-id}", studyProductId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(form))
+                .header("Authorization", AdminToken));
+
+        //then
+        result
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(STUDY_PRODUCT_DETAIL_CREATED))
+
+                .andDo(MockMvcRestDocumentation.document("studyproduct/create/detail",
+                        getDocumentRequest(), getDocumentResponse(),
+
+                        RequestDocumentation.pathParameters(
+                                RequestDocumentation.parameterWithName("study-product-id").description("스터디 상품 식별자")
+                        ),
+
+                        PayloadDocumentation.requestFields(
+                                fieldWithPath("[]title").type(JsonFieldType.STRING).description("스터디 상품 상세 주제")
+                        ),
+                        PayloadDocumentation.responseFields(
+                                fieldWithPath("status").type(JsonFieldType.NUMBER).description("상태 코드"),
+                                fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지")
+                        )
+                ));
     }
 }
