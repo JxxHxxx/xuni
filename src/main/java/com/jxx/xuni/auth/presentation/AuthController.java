fc69bc77877911365c47606f263c45f576ff763a
JxxHxxx <leesin5498@naver.com>
2023-05-03 16:21:41 +0900

    Feat/회원 가입 로직 이메일을 통한 본인 인증 추가 (#74)

--- src/main/java/com/jxx/xuni/auth/presentation/AuthController.java
+++ src/main/java/com/jxx/xuni/auth/presentation/AuthController.java
@@ -1,12 +1,15 @@
 package com.jxx.xuni.auth.presentation;
 
+import com.jxx.xuni.auth.application.AuthMailService;
 import com.jxx.xuni.auth.application.AuthService;
 import com.jxx.xuni.auth.application.MemberDetails;
+import com.jxx.xuni.auth.dto.request.AuthCodeForm;
 import com.jxx.xuni.auth.dto.request.LoginForm;
 import com.jxx.xuni.auth.dto.request.SignupForm;
 import com.jxx.xuni.auth.dto.response.LoginResponse;
 import com.jxx.xuni.auth.dto.response.AuthSimpleResult;
 import com.jxx.xuni.auth.support.JwtTokenProvider;
+import com.jxx.xuni.member.domain.Member;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,13 +24,21 @@ public class AuthController {
 
     private final AuthService authService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final AuthMailService authMailService;
 
     @PostMapping("/auth/signup-email")
-    public ResponseEntity<AuthSimpleResult> SignupForEmail(@RequestBody @Validated SignupForm signupForm) {
-        authService.signup(signupForm);
+    public ResponseEntity<AuthSimpleResult> signupForEmail(@RequestBody @Validated SignupForm signupForm) {
+        Member member = authService.signup(signupForm);
+        authMailService.sendAuthCode(member.receiveEmail());
         return new ResponseEntity(AuthSimpleResult.signup(), CREATED);
     }
 
+    @PatchMapping("/auth/verify-code")
+    public ResponseEntity<AuthSimpleResult> verifyAuthCode(@RequestBody AuthCodeForm authCodeForm) {
+        authMailService.verifyAuthCode(authCodeForm);
+
+        return ResponseEntity.ok(AuthSimpleResult.verified());
+    }
     @PostMapping("/auth/login")
     public ResponseEntity<AuthSimpleResult> login(@RequestBody LoginForm loginForm, HttpServletResponse response) {
         MemberDetails memberDetails = authService.login(loginForm);
---
+++ src/main/java/com/jxx/xuni/member/domain/UsageType.java
@@ -0,0 +1,5 @@
+package com.jxx.xuni.member.domain;
+
+public enum UsageType {
+    SIGNUP, PASSWORD_RESET
+}
