3d98426196d250b8329402aaa6237f400288e982
JxxHxxx <leesin5498@naver.com>
2023-05-11 16:24:40 +0900

    Fix/스프링 스케줄러 중단 및 Deprecated 선언 (#93)

--- src/main/java/com/jxx/xuni/group/domain/GroupStatusHandler.java
+++ src/main/java/com/jxx/xuni/group/domain/GroupStatusHandler.java
@@ -4,6 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/** 2023-05-11 BREAKING CHANGE : 그룹 스케줄러 책임 변경
+ *  그룹 상태에 스케줄러 관리는 이제 xuni-lambda 프로젝트에서 책임집니다.
+ *  https://github.com/JxxHxxx/xuni-lambda
+ */
+
+@Deprecated
 @Service
 @RequiredArgsConstructor
 public class GroupStatusHandler {
