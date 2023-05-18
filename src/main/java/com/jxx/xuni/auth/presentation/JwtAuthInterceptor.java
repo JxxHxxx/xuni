4ef2bd22de980c074e8f6bb3dfb18dc56c1564b2
JxxHxxx <leesin5498@naver.com>
2023-05-18 14:40:16 +0900

    Feat/jwt 인터셉터 기능 강화 (#106)
    1. 인증이 필요하지 않은 요청 조건 추가
    - HTTP 메서드 GET (기존)
    - Public URI Prefix 조건 (추가)
    2. 인증이 필요한 요청에 Authorization 헤더가 존재하지 않는 경우 예외 처리

--- src/main/java/com/jxx/xuni/auth/presentation/JwtAuthInterceptor.java
+++ src/main/java/com/jxx/xuni/auth/presentation/JwtAuthInterceptor.java
@@ -1,20 +1,24 @@
 package com.jxx.xuni.auth.presentation;
 
 import com.jxx.xuni.auth.support.JwtTokenManager;
+import io.jsonwebtoken.security.SecurityException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.HandlerInterceptor;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+
+import static com.jxx.xuni.common.exception.CommonExceptionMessage.REQUIRED_LOGIN;
 
 @RequiredArgsConstructor
 public class JwtAuthInterceptor implements HandlerInterceptor {
 
     private final JwtTokenManager jwtTokenManager;
+    private static final List<String> PRIVATE_ACCESS_URI_PREFIX = List.of("/members");
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (isGetMethod(request)) {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (notRequiredAuthentication(request)) {
             return true;
         }
 
@@ -23,11 +27,26 @@ public class JwtAuthInterceptor implements HandlerInterceptor {
         return true;
     }
 
+    private boolean notRequiredAuthentication(HttpServletRequest request) {
+        return isGetMethod(request) && isPublicURIPrefix(request);
+    }
+
     private boolean isGetMethod(HttpServletRequest request) {
         return "GET".equals(request.getMethod());
     }
 
+    private boolean isPublicURIPrefix(HttpServletRequest request) {
+        for (String domain : PRIVATE_ACCESS_URI_PREFIX) {
+            if (request.getRequestURI().startsWith(domain)) return false;
+        }
+        return true;
+    }
+
     private String extractAuthorizationHeader(HttpServletRequest request) {
-        return request.getHeader("Authorization").substring(7);
+        try {
+            return request.getHeader("Authorization").substring(7);
+        } catch (NullPointerException exception) {
+            throw new SecurityException(REQUIRED_LOGIN);
+        }
     }
 }
