c440b79724ad408bb5710b2fd435af87d09f53db
JxxHxxx <leesin5498@naver.com>
2023-05-18 16:26:16 +0900

    Feat/선택 인증 파라미터 애노테이션 추가 (#106)

---
+++ src/main/java/com/jxx/xuni/auth/presentation/OptionalAuthenticationArgumentResolver.java
@@ -0,0 +1,38 @@
+package com.jxx.xuni.auth.presentation;
+
+import com.jxx.xuni.auth.application.MemberDetails;
+import com.jxx.xuni.auth.application.SimpleMemberDetails;
+import com.jxx.xuni.auth.support.JwtTokenManager;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+public class OptionalAuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final JwtTokenManager jwtTokenManager;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginMemberAnnotation = parameter.hasParameterAnnotation(OptionalAuthentication.class);
+        boolean hasMemberDetailsType = MemberDetails.class.isAssignableFrom(parameter.getParameterType());
+
+        return hasLoginMemberAnnotation && hasMemberDetailsType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        try {
+            String token = request.getHeader("Authorization");
+            return jwtTokenManager.getMemberDetails(token.substring(7));
+        } catch (NullPointerException exception) {
+            return SimpleMemberDetails.GUEST();
+        }
+    }
+}
