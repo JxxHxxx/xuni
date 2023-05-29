255129d15c0f31b9198b9526d736f932078f9aba
JxxHxxx <leesin5498@naver.com>
2023-05-29 17:30:44 +0900

    Feat/스터디 상품 기본 조회 API 수정  (#140)
    1. Page 객체로 응답 default size 10 page 0
    2. 응답 본문에 스터디 상품 식별자 추가

--- src/main/java/com/jxx/xuni/studyproduct/dto/response/StudyProductReadResponse.java
+++ src/main/java/com/jxx/xuni/studyproduct/dto/response/StudyProductReadResponse.java
@@ -3,6 +3,7 @@ package com.jxx.xuni.studyproduct.dto.response;
 import com.jxx.xuni.common.domain.Category;
 
 public record StudyProductReadResponse(
+        String studyProductId,
         String name,
         Category category,
         String creator,
