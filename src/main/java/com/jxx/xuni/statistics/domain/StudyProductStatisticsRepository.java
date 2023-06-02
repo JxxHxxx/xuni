833db395463aba8eb672529a601669f86385d734
JxxHxxx <leesin5498@naver.com>
2023-06-03 02:38:45 +0900

    Feat/스터디 생성 Event 기능 추가 (#157)

---
+++ src/main/java/com/jxx/xuni/statistics/domain/StudyProductStatisticsRepository.java
@@ -0,0 +1,6 @@
+package com.jxx.xuni.statistics.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyProductStatisticsRepository extends JpaRepository<StudyProductStatistics, String> {
+}
