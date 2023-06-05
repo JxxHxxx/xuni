ed0abdd81b9eabe32ecd5124d4ebc6e47e676be3
JxxHxxx <leesin5498@naver.com>
2023-06-05 11:14:06 +0900

    Fix/누락된 클래스 등록 (#162)

--- src/main/java/com/jxx/xuni/statistics/dto/response/StatisticsApiResult.java
+++ src/main/java/com/jxx/xuni/statistics/dto/response/StatisticsApiResult.java
@@ -1,6 +1,6 @@
 package com.jxx.xuni.statistics.dto.response;
 
-public record MemberStatisticsApiResult<T>(
+public record StatisticsApiResult<T>(
         Integer status,
         String message,
         T response
