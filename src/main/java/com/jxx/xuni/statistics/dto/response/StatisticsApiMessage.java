ce886defd80a7a3d5ae7a5925b750236c7d35805
JxxHxxx <leesin5498@naver.com>
2023-06-07 19:33:57 +0900

    Refactor/클래스 명 변경 (#171)

--- src/main/java/com/jxx/xuni/statistics/dto/response/StatisticsApiMessage.java
+++ src/main/java/com/jxx/xuni/statistics/dto/response/StatisticsApiMessage.java
@@ -4,7 +4,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class MemberStatisticsApiMessage {
+public class StatisticsApiMessage {
 
     public static final String REVIEW_NEED_DATA = "리뷰 생성에 필요한 데이터 조회 완료";
 }
