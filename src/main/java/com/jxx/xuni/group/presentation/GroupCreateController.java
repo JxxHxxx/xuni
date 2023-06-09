2d49a45833d029d0d168bc798f300c41b8aa372a
JxxHxxx <leesin5498@naver.com>
2023-06-10 04:18:39 +0900

    Fix/사용하지 않는 의존성 제거 (#184)

--- src/main/java/com/jxx/xuni/group/presentation/GroupCreateController.java
+++ src/main/java/com/jxx/xuni/group/presentation/GroupCreateController.java
@@ -6,13 +6,11 @@ import com.jxx.xuni.group.application.GroupCreateService;
 import com.jxx.xuni.group.dto.request.GroupCreateForm;
 import com.jxx.xuni.group.dto.response.GroupApiSimpleResult;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.*;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class GroupCreateController {
@@ -20,8 +18,8 @@ public class GroupCreateController {
     private final GroupCreateService groupCreateService;
 
     @PostMapping("/groups")
-    public ResponseEntity<GroupApiSimpleResult> createV2(@AuthenticatedMember MemberDetails memberDetails,
-                                                         @RequestBody GroupCreateForm groupCreateForm) {
+    public ResponseEntity<GroupApiSimpleResult> create(@AuthenticatedMember MemberDetails memberDetails,
+                                                       @RequestBody GroupCreateForm groupCreateForm) {
 
         groupCreateService.create(memberDetails.getUserId(), groupCreateForm);
         return new ResponseEntity(GroupApiSimpleResult.createGroup(), CREATED);
